515db0c0739c65b5c16e037ee65a58d0ce86277f
ddecataldo <davidedecataldo92@gmail.com>
2021-11-17 16:39:44 +0100

    Inizio esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <!-- Campo di ricerca -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="input-group my-3">
+
+            <input type="text" class="form-control" placeholder="Inserisci il nome del film..." v-model="filmCercato">
+
+            <button class="btn btn-primary" type="button" id="button-addon2">Cerca</button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- Film -->
+          <ul>
+            <li v-for="film in listaFilm" :key="film.id">
+              {{ film.title }}
+            </li>
+          </ul>
+          <!-- Serie TV -->
+          <ul>
+            <li v-for="serieTv in listaSerieTv" :key="serieTv.id">
+              {{ serieTv.name }}
+            </li>
+          </ul>
+        </div>
+      </div>   
+    </div>
+  </div>  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from "axios";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      apiKey: "5d72883ce5a456a4913de11d19f67e70",
+      apiUrl: "https://api.themoviedb.org/3",
+      filmCercato: "",
+      listaFilm: [],
+      listaSerieTv: [],
+    };
+  },
+  methods: {
+    queryRicerca(url, testoDaCercare, chiave){
+      axios.get(this.apiUrl + url, {
+      params: {
+        api_key: this.apiKey,
+        query: testoDaCercare,
+        language: "it",
+      }
+      })
+      .then(res => {
+        this[chiave] = res.data.results;
+      });
+    }
+  },
+  mounted(){
+    this.queryRicerca("/search/movie", this.filmCercato, "listaFilm");
+    this.queryRicerca("/search/tv", this.filmCercato, "listaSerieTv");
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/scss/bootstrap";
 </style>
